7399b408bc3e8f3cb94d1842b49fdcce83083ab9
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-09-24 12:07:19 +0900

    boy circle

--- Rectanglemove.py
+++ Rectanglemove.py
@@ -1,6 +1,7 @@
 import pico2d
 import os
 from pico2d import *
+import math
 open_canvas()
 os.getcwd()
 
@@ -10,12 +11,19 @@ boy = load_image('character.png')
 open_canvas()
 
 def run_draw_circle():
- print("circle")
+  print("circle")
 
- clear_canvas_now()
- boy.draw_now(400,300)
- delay(1)
- pass#내용이 없는 함수
+  r=300
+
+  for degree in range(0,360,3):#각도 정의
+    theta = math.radians(degree)
+    x= r * math.cos(theta)
+    y= r * math.sin(theta)
+
+    clear_canvas_now()
+    boy.draw_now(x,y)
+    delay(1)
+  pass#내용이 없는 함수
 
 
 
